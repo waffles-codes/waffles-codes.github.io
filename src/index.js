40b7dc339f174fdf0be75d4c51a6cb0c573f1137
Evan Lu <evalu802@gmail.com>
2024-08-27 16:55:56 -0700

    last try of raw react

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from '@babel/preset-react';
+import ReactDOM from '@babel/preset-react';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 3000
+  }
+};
