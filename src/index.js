41c55e4f1ba1b9dd30817eda2a481dedc69a5268
Evan Lu <evalu802@gmail.com>
2024-08-28 01:56:05 -0700

    added more things to try and get ghpages to render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  HashRouter as Router,
+  Route,
+  } from 'react-router-dom'; 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +11,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router exact path="/">
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
