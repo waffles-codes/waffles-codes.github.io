e00810e8f8ff81358b0b40fd5b3d9623d98a27a4
Evan Lu <evalu802@gmail.com>
2024-08-28 19:19:54 -0700

    navbar centering and alignment optimizations

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,33 +6,33 @@ return (
     <nav className="navbar">
         <div className="navbar-left">
             <h1>
-                Evan Lu
+                Evan&nbsp;Lu
             </h1>
         </div>
 
         <div className="navbar-center">
             <ul className="nav-links">
-            <li>
-                <a href="/about">About Me</a>
-            </li>
-            <li>
-                <a href="/projects">Projects</a>
-            </li>
-            <li>
-                <a href="/contact">Contact</a>
-            </li>
+                <li>
+                    <a href="/about">About&nbsp;Me</a>
+                </li>
+                <li>
+                    <a href="/projects">Projects</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
             </ul>
         </div>
         
         <div className="navbar-right">
-            <a href="mailto:evalu802@gmail.com" className="image-link">
+            <a href="mailto:evalu802@gmail.com" className="image-links">
                 <img src="email.svg" width="20"/>
             </a>
-            <a href="https://www.linkedin.com/in/waffles-codes/" className="image-link">
-                <img src="linkedin.svg" width="20"></img>
+            <a href="https://www.linkedin.com/in/waffles-codes/" className="image-links">
+                <img src="linkedin.svg" width="20"/>
             </a>
-            <a href="https://github.com/waffles-codes" className="image-link">
-                <img src="github.svg" width="20"></img>
+            <a href="https://github.com/waffles-codes" className="image-links">
+                <img src="github.svg" width="20"/>
             </a>
         </div>
     </nav>
