dbc2515d9d68b3e2a431d317471c30825db15165
Evan Lu <evalu802@gmail.com>
2024-09-04 00:53:49 -0700

    put /# in links (navbar): hashrouter for ghpages

--- src/Navbar.js
+++ src/Navbar.js
@@ -16,10 +16,10 @@ return (
                     <a href="/">Home</a>
                 </li>
                 <li>
-                    <a href="/projects">Projects</a>
+                    <a href="/#/projects">Projects</a>
                 </li>
                 <li>
-                    <a href="/contact">Contact</a>
+                    <a href="/#/contact">Contact</a>
                 </li>
             </ul>
         </div>
