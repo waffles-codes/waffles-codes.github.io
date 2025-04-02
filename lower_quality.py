04ab6796ec27f926fb249ded0072f189f19f3d19
waffles-codes <evalu802@gmail.com>
2025-04-02 01:40:44 -0700

    cropped pfp

--- lower_quality.py
+++ lower_quality.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-file = "Evan_Lu_Professional_Profile_Confident.webp"
+file = "Evan_Lu_Professional_Profile_Confident_Cropped.webp"
 
 directory = "D:/Programming/Github Desktop/waffles-codes.github.io/src/assets"
 print(f'Current working directory = {directory}')
